da0d9313ad328b4b0a8aede8b961d547af349ec9
cybrooke <maneaiulian96@yahoo.com>
2018-03-06 22:52:44 +0000

    fix

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -3,7 +3,10 @@ import { h, render, Component } from 'preact';
 // import stylesheets for ipad & button
 import style from './style';
 import style_iphone from '../button/style_iphone';
+<<<<<<< Updated upstream
 import style_button from '../button/style_button';
+=======
+>>>>>>> Stashed changes
 import refresh from '../../assets/icons/refresh.png';
 import sbarImg from '../../assets/icons/sidebar.png';
 import widImg from '../../assets/icons/weather_icons/snowy_copy.png';
@@ -20,33 +23,46 @@ import Button from '../button';
 export default class Iphone extends Component {
 //var Iphone = React.createClass({
 
+
+
 	// a constructor with initial set states
 	constructor(props){
 		super(props);
 		// temperature state
+		this.state.daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 		this.state.temp = "";
-		this.state.date = Date.now();
+		this.state.date = new Date();
 		// button display state
 		this.setState({ display: true });
 	}
 
+	//this method is called when an instance of a component is being created and inserted into the DOM
+	componentWillMount() {
+		this.fetchWeatherData();
+	}
 
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
 		var snowReportUrl = "http://api.weatherunlocked.com/api/snowreport/54883461?app_id=3962eedb&app_key=fda3e707923d6caa68bd2cba54a400f3";
 		var resortForecastUrl = "http://api.weatherunlocked.com/api/resortforecast/54883461?app_id=3962eedb&app_key=fda3e707923d6caa68bd2cba54a400f3"
+		var astronomyInfUrl = "http://api.wunderground.com/api/221d2d3d52cb8a39/astronomy/q/Finland/Levi.json"
+
 		$.ajax({
 			url: resortForecastUrl,
-
 			success : this.parseResortForecast,
 			error : function(req, err){ console.log('API call failed ' + err); }
 		})
 
 		$.ajax({
 			url: snowReportUrl,
-
 			success : this.parseSnowReport,
+			error : function(req, err){ console.log('API call failed ' + err); }
+		})
+
+		$.ajax({
+			url: astronomyInfUrl,
+			success : this.parseAstronomyInformation,
 			error : function(req, err){ console.log('API call failed ' + err); }
 		})
 
@@ -61,14 +77,12 @@ export default class Iphone extends Component {
 		const tempStyles = this.state.temp ? `${style.temperature} ${style.filled}` : style.temperature;
 		// display all weather data
 
+
+
 		return (
 			<div class={ style.container } >
 
-					<div class={ style.header }>
-						<img src={sbarImg} class={ style_button.sidebar }/>
-						<p>{this.state.locate}</p>
-						<img src={refresh} class={ style_button.refresh } onClick={ this.fetchWeatherData }/>
-					</div>
+
 
 					<div class={style.appName}>
 					<h3>SkiWe</h3>
@@ -83,7 +97,7 @@ export default class Iphone extends Component {
 									</div>
 									<hr/>
 									<div class={style.data2}>
-									Feels like {this.state.feels}<sup>o</sup>
+									Feels like {this.state.feels_like}<sup>o</sup>
 									</div>
 									</div>
 								<div class={style.twm}>
@@ -97,13 +111,14 @@ export default class Iphone extends Component {
 							</div>
 							<div class={style.middle_widget }>
 							<div class={style.date}>
-							{this.state.date}
+							{this.state.date.toLocaleDateString()}
 							</div>
 							<div class={style.time}>
-							{this.state.time}
+							{this.state.date.toLocaleTimeString('en-GB', { hour: "numeric",
+                                             minute: "numeric"})}
 							</div>
 							<div class={style.date}>
-							{this.state.day}
+							{this.state.daysOfWeek[this.state.date.getDay()]}
 							</div>
 							</div>
 							<hr/>
@@ -114,7 +129,7 @@ export default class Iphone extends Component {
 								</div>
 								<div class={style.bwi}>
 								<img src={windChill}/>
-								<div>{this.state.windchill}<sup>o</sup></div>
+								<div>{this.state.wind}<sup>km/h</sup></div>
 								</div>
 								<div class={style.bwc}>
 								</div>
@@ -146,7 +161,7 @@ export default class Iphone extends Component {
 							</div>
 							<hr class={style.body_info_2}/>
 							<div class={style.body_info_3}>
-							<h4>{this.state.cond}</h4>
+							<h4>{this.state.lowersnow}cm</h4>
 							</div>
 							</div>
 							<div class={style.body_info}>
@@ -155,7 +170,7 @@ export default class Iphone extends Component {
 							</div>
 							<hr class={style.body_info_2}/>
 							<div class={style.body_info_3}>
-							<h4>{this.state.cond}</h4>
+							<h4>{this.state.newsnow}cm</h4>
 							</div>
 							</div>
 							<div class={style.body_info}>
@@ -164,7 +179,7 @@ export default class Iphone extends Component {
 							</div>
 							<hr class={style.body_info_2}/>
 							<div class={style.body_info_3}>
-							<h4>{this.state.cond}</h4>
+							<h4>{this.state.conditions}</h4>
 							</div>
 							</div>
 							<div class={style.body_info}>
@@ -186,24 +201,9 @@ export default class Iphone extends Component {
 							<h3 class={style.footer_day}>Day</h3>
 						</div>
 						<div class={style.footer_details}>
-							<div class={style.footer_info}>
-							1
-							</div>
-							<div class={style.footer_info}>
-							2
-							</div>
-							<div class={style.footer_info}>
-							3
-							</div>
-							<div class={style.footer_info}>
-							4
-							</div>
-							<div class={style.footer_info}>
-							5
-							</div>
-							<div class={style.footer_info}>
-							6
-							</div>
+						{}
+							{this.state.hourly_temp}
+
 						</div>
 					</div>
 
@@ -218,6 +218,19 @@ export default class Iphone extends Component {
 		var feels_like = parsed_json['forecast'][0]['base']['feelslike_c'];
 		var relative_humidity = parsed_json['forecast'][0]['hum_pct'];
 		var wind_speed = parsed_json['forecast'][0]['base']['windspd_kmh'];
+		var hourly_temp = [];
+
+		for(var i = 0; i < 6; i++) {
+			hourly_temp.push(parsed_json['forecast'][i+1]['base']['temp_c']);
+		}
+
+		var hourly_temp_html = hourly_temp.map(function(temp) {
+			return (
+				<div class={style.footer_info}>
+					{temp}
+				</div>
+			)
+		})
 
 		//var hourly = parsed_json['hourly_forecast'].map((result) => <HourlyResult result={result}/>);
 		// set states for fields so they could be rendered later on
@@ -227,7 +240,7 @@ export default class Iphone extends Component {
 			feels_like: feels_like,
 			humidity: relative_humidity,
 			wind: wind_speed,
-			//hourly_cond: hourly
+			hourly_temp: hourly_temp_html
 		});
 
 	}
@@ -236,10 +249,22 @@ export default class Iphone extends Component {
 	parseSnowReport = (parsed_json) => {
 		var conditions = parsed_json['conditions'];
 		var lowersnow_cm = parsed_json['lowersnow_cm'];
+		var newsnow_cm = parsed_json['newsnow_cm'];
 
 		this.setState({
 			conditions: conditions,
-			lowersnow: lowersnow_cm
+			lowersnow: lowersnow_cm,
+			newsnow: newsnow_cm
+		})
+	}
+
+	parseAstronomyInformation = (parsed_json) => {
+		var sunrise = parsed_json['sun_phase']['sunrise']['hour'] + ":" + parsed_json['sun_phase']['sunrise']['minute'];
+		var sunset = parsed_json['sun_phase']['sunset']['hour'] + ":" + parsed_json['sun_phase']['sunset']['minute'];
+
+		this.setState({
+			sunrise: sunrise,
+			sunset: sunset
 		})
 	}
 
